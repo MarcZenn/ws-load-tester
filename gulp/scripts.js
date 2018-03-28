abe424598d2a6117a95030f1acf8365b43ebe792
Marc <marcusareulius@gmail.com>
2018-03-28 15:01:05 +0200

    angular node boilerplate packages updated, compiling and serving via gulp

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -1,26 +1,25 @@
-'use strict';
+"use strict";
 
-var path = require('path');
-var gulp = require('gulp');
-var conf = require('./conf');
+var path = require("path");
+var gulp = require("gulp");
+var conf = require("./conf");
 
-var browserSync = require('browser-sync');
+var browserSync = require("browser-sync");
 
-var $ = require('gulp-load-plugins')();
+var $ = require("gulp-load-plugins")();
 
-
-gulp.task('scripts-reload', function() {
-  return buildScripts()
-    .pipe(browserSync.stream());
+gulp.task("scripts-reload", function() {
+  return buildScripts().pipe(browserSync.stream());
 });
 
-gulp.task('scripts', function() {
+gulp.task("scripts", function() {
   return buildScripts();
 });
 
 function buildScripts() {
-  return gulp.src(path.join(conf.paths.src, '/app/**/*.js'))
-    .pipe($.eslint())
+  return gulp
+    .src(path.join(conf.paths.src, "/app/**/*.js"))
+    .pipe($.eslint({ configFile: "gulp/.eslintrc" }))
     .pipe($.eslint.format())
-    .pipe($.size())
-};
+    .pipe($.size());
+}
